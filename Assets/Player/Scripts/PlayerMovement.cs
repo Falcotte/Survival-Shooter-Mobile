795927a50fa0c25404a4716727eb5291ea654321
Ali Unal <0033484@gmail.com>
2024-04-14 17:53:47 +0300

    Player movement added

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using SurvivalShooter.Inputs;
+using SurvivalShooter.Services;
+using UnityEngine;
+
+namespace SurvivalShooter.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private Animator _animator;
+
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _rotateSpeed;
+
+        private Vector3 _movementDirection;
+        private bool _isMoving;
+
+        private IInputService _inputService;
+        private IInputController _inputController;
+
+        private void Start()
+        {
+            _inputService = ServiceLocator.Get<IInputService>();
+            _inputController = _inputService.GetController(InputControllerType.Mobile);
+        }
+
+        private void Update()
+        {
+            _isMoving = _inputController.Direction.sqrMagnitude > 0;
+
+            if(_isMoving)
+            {
+                _movementDirection = _inputController.Direction;
+            }
+
+            Turn(_movementDirection);
+            _animator.SetBool("IsWalking", _isMoving);
+        }
+
+        private void FixedUpdate()
+        {
+            Move(_inputController.Direction);
+        }
+
+        private void Move(Vector2 input)
+        {
+            _rigidbody.MovePosition(_rigidbody.position + new Vector3(input.x, 0f, input.y) * _moveSpeed * Time.fixedDeltaTime);
+        }
+
+        private void Turn(Vector2 direction)
+        {
+            if(direction.sqrMagnitude > 0)
+            {
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, new Vector3(direction.x, 0f, direction.y).normalized, _rotateSpeed * Time.deltaTime, 0f);
+                transform.LookAt(transform.position + newDirection);
+            }
+        }
+    }
+}
