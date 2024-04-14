d54efe5e6586333fdf28b0a275f8396ce17ec9e7
Ali Unal <0033484@gmail.com>
2024-04-14 22:06:51 +0300

    Enemy spawning implemented

--- Assets/Enemy/Scripts/EnemyManager.cs
+++ Assets/Enemy/Scripts/EnemyManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace SurvivalShooter.Enemy
@@ -6,5 +7,30 @@ namespace SurvivalShooter.Enemy
     {
         [SerializeField] private Transform _playerTransform;
         public Transform PlayerTransform => _playerTransform;
+
+        [SerializeField] private List<EnemyController> _enemyControllerPrefabs = new();
+        [SerializeField] private List<Transform> _spawnPoints = new();
+
+        [SerializeField] private float _spawnFrequency;
+        private float _spawnTimer;
+
+        private void Update()
+        {
+            _spawnTimer += Time.deltaTime;
+
+            if(_spawnTimer >= _spawnFrequency)
+            {
+                SpawnEnemy();
+                _spawnTimer = 0f;
+            }
+        }
+
+        public void SpawnEnemy()
+        {
+            var enemyController = Instantiate(_enemyControllerPrefabs[Random.Range(0, _enemyControllerPrefabs.Count)], transform);
+            enemyController.transform.position = _spawnPoints[Random.Range(0, _spawnPoints.Count)].position;
+
+            enemyController.PlayerTransform = _playerTransform;
+        }
     }
 }
